7c59103ab7a5f56c2ee25e40b254e9c75c32cf03
Lisandro Gomez <Lozandrogomez@gmail.com>
2025-06-10 20:12:04 -0300

    actualizacion de modelo "crianza"

--- Models/Crianza.py
+++ Models/Crianza.py
@@ -1,3 +1,13 @@
+from uuid import uuid4
+from datetime import date
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def generate_uuid():
+    return str(uuid4())
+
+
 class Crianza(db.Model):
     __tablename__ = 'crianzas'
 
@@ -6,3 +16,5 @@ class Crianza(db.Model):
     tipo_recipiente = db.Column(db.String(100), nullable=False)
     fecha_inicio = db.Column(db.Date, nullable=False)
     observaciones = db.Column(db.Text)
+
+lote = db.relationship('lote_uva', backref=db.backref('crianza', lazy=True))
