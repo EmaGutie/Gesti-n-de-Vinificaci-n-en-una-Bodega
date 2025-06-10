59ff88af4df3e4b4d36f4ffeaa25143a0dd7792a
Lisandro Gomez <Lozandrogomez@gmail.com>
2025-06-10 20:38:03 -0300

    actualizacion de modelo de crianza y embotellado, ahora la fecha se pone automaticamente a la hora actual

--- Models/Crianza.py
+++ Models/Crianza.py
@@ -14,7 +14,7 @@ class Crianza(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
     lote_id = db.Column(db.String(36), db.ForeignKey('lotes_uva.id'), nullable=False)
     tipo_recipiente = db.Column(db.String(100), nullable=False)
-    fecha_inicio = db.Column(db.Date, nullable=False)
+    fecha_inicio = db.Column(db.Date, nullable=False, default=date.today)
     observaciones = db.Column(db.Text)
 
 lote = db.relationship('lote_uva', backref=db.backref('crianza', lazy=True))
--- Models/embotellado.py
+++ Models/embotellado.py
@@ -1,8 +1,17 @@
+from uuid import uuid4
+from datetime import date
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def generate_uuid():
+    return str(uuid4())
+
 class Embotellado(db.Model):
     __tablename__ = 'embotellados'
 
     id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
     lote_id = db.Column(db.String(36), db.ForeignKey('lotes_uva.id'), nullable=False)
-    fecha_embotellado = db.Column(db.Date, nullable=False)
+    fecha_embotellado = db.Column(db.Date, nullable=False, default=date.today)
     cantidad_botellas = db.Column(db.Integer)
     notas = db.Column(db.Text)
