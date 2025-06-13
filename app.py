df5ebb83a365078426db0de1e7ae427e91d680db
leonardo1330 <leonardoechevarria123@gmail.com>
2025-06-13 18:39:54 -0300

    subo datos

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ import os
 import uuid
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from werkzeug.utils import secure_filename 
-
+from routes.routes_embotellado import embotellado_bp
+from routes.routes_vinos import vinos_bp
+from routes.ruta_crianza import crianza_bp
+from routes.ruta_fermentacion import fermentacion_bp
+from routes.ruta_lote_uva import lote_bp
+from config.data import DATA_BASE_URL
 app = Flask(__name__)
+
+
+app=Flask(__name__)
+app.config["DATA_BASE_URL"]=DATA_BASE_URL
+app.config["DATA_TRACK_MODIFICATION"]=False
+app.register_blueprint(embotellado_bp)
+app.register_blueprint(vinos_bp)
+app.register_blueprint(crianza_bp)
+app.register_blueprint(fermentacion_bp)
+app.register_blueprint(lote_bp)
 
 UPLOAD_FOLDER = 'static/uploads'
 
